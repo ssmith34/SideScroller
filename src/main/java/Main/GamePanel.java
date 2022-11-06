c9c8e7c2fab6bdbe704078d09ecd9e0c48f36bf2
Shane Smith <slsmith0906@gmail.com>
2022-11-05 21:29:56 -0400

    Sprite imported

--- src/main/java/Main/GamePanel.java
+++ src/main/java/Main/GamePanel.java
@@ -3,43 +3,75 @@ package Main;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    private BufferedImage[] idleAni;
 
     public GamePanel() {
 
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+        loadAnimations();
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
     }
 
+    private void loadAnimations() {
+        idleAni = new BufferedImage[5];
+
+        for (int i = 0; i <idleAni.length; i++) {
+            idleAni[i] = img.getSubimage(i*64, )
+        }
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setPreferredSize(size);
+    }
+
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
-    }
-
-    public void setRectPos(int x, int y) {
-        this.xDelta = x;
-        this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+//        g.drawImage(subImg, (int)xDelta, (int)yDelta, 128, 80, null);
     }
 }
--- src/main/java/Main/GameWindow.java
+++ src/main/java/Main/GameWindow.java
@@ -9,10 +9,11 @@ public class GameWindow {
 
         jframe = new JFrame();
 
-        jframe.setSize(800, 800);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
         jframe.setLocationRelativeTo(null);
+        jframe.setResizable(false);
+        jframe.pack();
         jframe.setVisible(true);
     }
 }
