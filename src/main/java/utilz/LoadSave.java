ca4d035d7ac48894054b32f9e9a113bab5f5e4bf
Shane Smith <slsmith0906@gmail.com>
2022-11-14 09:26:50 -0500

    continued building enemy data

--- src/main/java/utilz/LoadSave.java
+++ src/main/java/utilz/LoadSave.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import entities.Crabby;
 import main.Game;
+
+import static utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
@@ -43,6 +47,20 @@ public class LoadSave {
 			}
 		}
 		return img;
+	}
+
+	public static ArrayList<Crabby> GetCrabs() {
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		ArrayList<Crabby> list = new ArrayList<>();
+
+		for (int j = 0; j < img.getHeight(); j++)
+			for (int i = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getGreen();
+				if (value == CRABBY)
+					list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE));
+			}
+		return list;
 	}
 
 	public static int[][] GetLevelData() {
