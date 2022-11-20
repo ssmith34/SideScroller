4c5ca23239f56e458afa3c11c3f6f1d490524398
Shane Smith <slsmith0906@gmail.com>
2022-11-20 14:33:24 -0500

    Added attacking option for enemies

--- src/main/java/entities/Crabby.java
+++ src/main/java/entities/Crabby.java
@@ -1,10 +1,35 @@
 package entities;
 
+import main.Game;
 import static utilz.Constants.EnemyConstants.*;
 
-public class Crabby extends Enemy{
+public class Crabby extends Enemy {
 
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+        initHitbox(x, y, (int) (22 * Game.SCALE), (int) (19 * Game.SCALE));
+    }
+
+    public void update(int[][] lvlData) {
+        updateMove(lvlData);
+        updateAnimationTick();
+    }
+
+    private void updateMove(int[][] lvlData) {
+        if (firstUpdate)
+            firstUpdateCheck(lvlData);
+
+        if (inAir)
+            updateInAir(lvlData);
+        else {
+            switch (enemyState) {
+                case IDLE:
+                    newState(RUNNING);
+                    break;
+                case RUNNING:
+                    move(lvlData);
+                    break;
+            }
+        }
     }
 }
